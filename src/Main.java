fd657303f998b6a8654186fef6b42d93c8394f53
Aleksey29247 <alik29247@gmail.com>
2021-12-26 17:32:48 +0700

    AAA

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main (String [] args) {
+        int  сlientAccount = 100; // остаток на чсете клиента
+        System.out.println ("Начальный счет:" + сlientAccount);
+        int accountReplenished  = 100; // сумма пополнения
+        int bonus=0;
+        int totalAccount =0;
+        if  (accountReplenished>=1000)
+        {
+            bonus=accountReplenished/100;
+            totalAccount = сlientAccount+accountReplenished+bonus;
+            System.out.println ("Итоговый счет с бонусоми =" + totalAccount);
+        }
+        else
+        {
+            totalAccount = сlientAccount+accountReplenished;
+            System.out.println ("Итоговый счет без бонусов =" + totalAccount);
+        }
+        accountReplenished  = 2000; // сумма пополнения
+        if  (accountReplenished>=1000)
+        {
+
+            bonus=accountReplenished/100;
+            totalAccount = сlientAccount+accountReplenished+bonus;
+            System.out.println ("Итоговый счет с бонусоми =" + totalAccount);
+        }
+        else
+        {
+            totalAccount = сlientAccount+accountReplenished;
+            System.out.println ("Итоговый счет без бонусов =" + totalAccount);
+        }
+
+
+
+
+    }
+
+}
